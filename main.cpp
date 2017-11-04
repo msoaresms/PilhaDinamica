bdb592f090423bd74df7a095e5da5c386ac11c09
Matheus Soares <m.soares.ms@outlook.com>
2017-11-04 00:11:07 -0400

    testes

--- main.cpp
+++ main.cpp
@@ -1,59 +1,9 @@
 #include <iostream>
 #include "Item.h"
+#include "Pilha.h"
 using namespace std;
 
-template <class T>
-class Pilha{
-private:
-    No<T> *topo, *fundo;
-    bool vazia() {
-        return (this->topo == this->fundo);
-    }
-public:
-    Pilha(){
-        this->fundo = new No<T>();
-        this->fundo->setProx(NULL);
-        this->topo = this->fundo;
-    }
-
-    No<T> *getTopo() const {
-        return topo;
-    }
-    void setTopo(No<T> *topo) {
-        this->topo = topo;
-    }
-    No<T> *getFundo() const {
-        return fundo;
-    }
-    void setFundo(No<T> *fundo) {
-        this->fundo = fundo;
-    }
-
-    void empilha(T);
-    void desempilha(T *);
-};
-
-
-
-template <typename T>
-void Pilha<T>::empilha(T pItem) {
-    No<T> *aux = new No<T>();
-    this->topo->setItem(pItem);
-    aux->setProx(this->topo);
-    this->topo = aux;
-}
-
-template <typename T>
-void Pilha<T>::desempilha(T *x) {
-    if (!this->vazia()) {
-        No<T> *aux = this->topo;
-        this->topo = this->topo->getProx();
-        *x = this->topo->getItem();
-        delete aux;
-    }
-}
-
-int main(int argc, const char * argv[]){
+int main(){
     Pilha<Item> p;
     Item x;
 
